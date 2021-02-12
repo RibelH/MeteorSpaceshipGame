d997a68cd2c5dd15e07125e43c719bb42390c75a
RÖBI <robert.hemenguel@hotmail.com>
2021-02-12 20:54:12 +0100

    Ver. 0.0.2 -------------- - Optimizations - updated Player-Sprite - added Shooting and Bullets

--- main.py
+++ main.py
@@ -12,16 +12,44 @@ win = pygame.display.set_mode((screen_w,screen_h))
 pygame.display.set_caption("First Game")
 
 #Player Position
-x = 50
-y = 50
+
 
 #Player size
-player = pygame.image.load("char/PurpleSpaceShip2.png")
-width = 16
-height = 16
+player = pygame.image.load("char/PurpleSpaceship3.png")
+
 
 #Player speed/directional change
-vel = 5
+
+
+
+#OBJECT CLASSES
+
+class player(object):
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.vel = 4
+        self.player = pygame.image.load("char/PurpleSpaceship3.png")
+
+    def draw(self,win):
+        win.blit(self.player, (self.x, self.y))
+
+class projectile(object):
+
+    def __init__(self, x, y, radius, color):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.color = color
+        self.vel = -8
+        #self.projectile = pygame.image.load("projectiles/bullet.png", )
+
+    def draw(self, win):
+        #win.blit(self.projectile, (self.x, self.y)
+        pygame.draw.circle(win, self.color, (self.x, self.y), self.radius)
+
 
 
 def RedrawGameWindow():
@@ -31,17 +59,27 @@ def RedrawGameWindow():
 
     #Draw Player
     #pygame.draw.rect(win, (255,255,255), (x, y, width, height))
-    win.blit(player, (x,y))
+    ship.draw(win)
+
+    for bullet in bullets:
+        bullet.draw(win)
 
     #Updating Window
     pygame.display.update()
 
 
 #Loop for Game
+ship = player(300, 410, 64, 64)
+shootLoop = 0
+bullets = []
 run = True
 while run:
     #Delay for Loop
     pygame.time.delay(15)
+    if shootLoop > 0:
+        shootLoop += 1
+    if shootLoop > 3:
+        shootLoop = 0
 
     #Checking for events
     for event in pygame.event.get():
@@ -50,29 +88,41 @@ while run:
         if event.type == pygame.QUIT:
             run = False
 
+    for bullet in bullets:
+        if bullet.y > 0 and bullet.y < 500:
+            bullet.y += bullet.vel
+        else:
+            bullets.pop(bullets.index(bullet))
+
+
     #Gets the state of ALL keys on the keyboard
     keys = pygame.key.get_pressed()
 
     #Checking for Key inputs
+    if keys[pygame.K_SPACE] and shootLoop == 0:
+        if len(bullets) < 100:
+            bullets.append(projectile(round(ship.x + ship.width//2), round(ship.y + ship.height//4) , 6, (255,255,255)))
+        shootLoop = 1
+
     if keys[pygame.K_LEFT]:
         #Checking for Left-Border
-        if x > 0:
-            x -= vel
+        if ship.x > 0:
+            ship.x -= ship.vel
     if keys[pygame.K_RIGHT]:
         #Checking for Right-Border
-        if x < (screen_w - width):
-            x += vel
+        if ship.x < (screen_w - ship.width):
+            ship.x += ship.vel
     if keys[pygame.K_DOWN]:
         #Checking for Bottom-Border
-        if y < (screen_h - width):
-            y += vel
+        if ship.y < (screen_h - ship.width):
+            ship.y += ship.vel
     if keys[pygame.K_UP]:
         #Checking for Top-Border
-        if y > 0:
-            y -= vel
+        if ship.y > 0:
+            ship.y -= ship.vel
 
-    if keys[pygame.K_SPACE]:
-        pass
+
+
 
     RedrawGameWindow()
 
